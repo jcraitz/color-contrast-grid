8583261e1c09f95acca210ae3b0aefaba6862101
jcraitz <jcraitz@ncsu.edu>
2019-11-01 16:21:13 -0400

    added button to check contrast

--- colors.js
+++ colors.js
@@ -13,11 +13,48 @@ const colorsArr = Object.entries(colors).reduce((acc, [name, hex]) => {
   const colorObject = {
     name,
     hex,
+    hexs: hex.slice(1),
     // hsl: hexToHSL(hex),
     // compColor: findComp(hex),
   }
   return [...acc, colorObject];
 }, []);
+
+Vue.component('contrast-result', {
+  template: '#contrast-template',
+  props: {
+    xcolors: String,
+    rowColor: Object,
+    colColor: Object,
+  },
+  data: function(){
+    return {
+      rdata:{},
+    }
+  },
+  methods: {
+    contrast: function(event) {
+      let bc = this.rgb2hex(window.getComputedStyle(event.target.closest('td'))['background-color']);
+      let c = this.rgb2hex(window.getComputedStyle(event.target.closest('td'))['color']);
+      console.info(`background-color is ${bc}`);
+      console.info(`color is ${c}`);
+      fetch(`https://webaim.org/resources/contrastchecker/?fcolor=${c}&bcolor=${bc}&api`)
+        .then(response => response.json())
+        .then(json => this.rendercontrast(event.target,json));
+    },
+    rgb2hex: function(c) {
+      return c.match(/\d+/g).map(x=>(+x).toString(16).padStart(2,0)).join``;
+    },
+    rendercontrast: function(node,data){
+      this.rdata = data;
+      console.log(node);
+      console.log(data);
+    },
+    passRatio: function(ratio) {
+      return (ratio > 7)? 'pass':'fail';
+    }
+  },
+});
 
 Vue.component('color-grid', {
   template: '#grid-template',
@@ -28,9 +65,12 @@ Vue.component('color-grid', {
   // data: function() {
   //   //
   // },
-  // computed: {},
+  // computed: {
+
+  // },
   // filters: {},
-  // methods: {},
+  // methods: {
+  // },
 });
 
 var app = new Vue({
@@ -39,4 +79,7 @@ var app = new Vue({
     colorsArr: colorsArr,
     sample: 'sample text',
   }
+  // mounted() {
+  //   if(localStorage.)
+  // },
 })
