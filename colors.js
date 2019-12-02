c7b260927068699c9fb0bafd46fa27a2b76e4237
jcraitz <jcraitz@ncsu.edu>
2019-12-02 13:14:01 -0500

    eslint changes and slight styling

--- colors.js
+++ colors.js
@@ -8,7 +8,12 @@ const colors = {
   CarmichaelAqua: '#008473',
   InnovationBlue: '#427E93',
   BioIndigo: '#4156A1',
-}
+};
+const luminanceFactors = {
+  R: 0.2126,
+  G: 0.7152,
+  B: 0.0722,
+};
 const colorsArr = Object.entries(colors).reduce((acc, [name, hex]) => {
   const colorObject = {
     name,
@@ -16,7 +21,7 @@ const colorsArr = Object.entries(colors).reduce((acc, [name, hex]) => {
     hexs: hex.slice(1),
     // hsl: hexToHSL(hex),
     // compColor: findComp(hex),
-  }
+  };
   return [...acc, colorObject];
 }, []);
 
@@ -27,32 +32,38 @@ Vue.component('contrast-result', {
     rowColor: Object,
     colColor: Object,
   },
-  data: function(){
+  data() {
     return {
-      rdata:{},
-    }
+      rdata: {},
+    };
   },
   methods: {
-    contrast: function(event) {
-      let bc = this.rgb2hex(window.getComputedStyle(event.target.closest('td'))['background-color']);
-      let c = this.rgb2hex(window.getComputedStyle(event.target.closest('td'))['color']);
+    contrast(event) {
+      const bc = this.rgb2hex(
+        window.getComputedStyle(event.target.closest('td'))['background-color']
+      );
+      const c = this.rgb2hex(
+        window.getComputedStyle(event.target.closest('td')).color
+      );
       console.info(`background-color is ${bc}`);
       console.info(`color is ${c}`);
-      fetch(`https://webaim.org/resources/contrastchecker/?fcolor=${c}&bcolor=${bc}&api`)
+      fetch(
+        `https://webaim.org/resources/contrastchecker/?fcolor=${c}&bcolor=${bc}&api`
+      )
         .then(response => response.json())
-        .then(json => this.rendercontrast(event.target,json));
+        .then(json => this.rendercontrast(event.target, json));
     },
-    rgb2hex: function(c) {
-      return c.match(/\d+/g).map(x=>(+x).toString(16).padStart(2,0)).join``;
+    rgb2hex(c) {
+      return c.match(/\d+/g).map(x => (+x).toString(16).padStart(2, 0)).join``;
     },
-    rendercontrast: function(node,data){
+    rendercontrast(node, data) {
       this.rdata = data;
       console.log(node);
       console.log(data);
     },
-    passRatio: function(ratio) {
-      return (ratio > 7)? 'pass':'fail';
-    }
+    passRatio(ratio) {
+      return ratio > 7 ? 'pass' : 'fail';
+    },
   },
 });
 
@@ -73,13 +84,13 @@ Vue.component('color-grid', {
   // },
 });
 
-var app = new Vue({
+const app = new Vue({
   el: '#app',
   data: {
-    colorsArr: colorsArr,
+    colorsArr,
     sample: 'sample text',
-  }
+  },
   // mounted() {
   //   if(localStorage.)
   // },
-})
+});
